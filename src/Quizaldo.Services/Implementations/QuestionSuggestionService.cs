cb6c989c8b934e21fc9b63f8dd2aedc7f309f98b
Valentinles <valentinvn@abv.bg>
2020-06-17 12:39:55 +0300

    QuestionSuggestion service and interface added

---
+++ src/Quizaldo.Services/Implementations/QuestionSuggestionService.cs
@@ -0,0 +1,86 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Quizaldo.Data;
+using Quizaldo.Models;
+using Quizaldo.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizaldo.Services.Implementations
+{
+    public class QuestionSuggestionService : DataService, IQuestionSuggestionService
+    {
+        private readonly IMapper mapper;
+        public QuestionSuggestionService(QuizaldoDbContext context, IMapper mapper) : base(context)
+        {
+            this.mapper = mapper;
+        }
+
+        public async Task SuggestQuestion(QuestionSuggestion questionSuggestion, string username)
+        {
+            var user = await this.context.Users.FirstOrDefaultAsync(u => u.UserName == username);
+
+            questionSuggestion.User = user;
+            questionSuggestion.SuggestedOn = DateTime.Now;
+
+            if (questionSuggestion.CorrectAnswer == questionSuggestion.FirstOption ||
+                questionSuggestion.CorrectAnswer == questionSuggestion.SecondOption ||
+                questionSuggestion.CorrectAnswer == questionSuggestion.ThirdOption ||
+                questionSuggestion.CorrectAnswer == questionSuggestion.FourthOption)
+            {
+                await this.context.QuestionSuggestions.AddAsync(questionSuggestion);
+            }
+            else
+            {
+                return;
+            }
+
+            await this.context.SaveChangesAsync();
+        }
+
+        public async Task ApproveSuggestion(int questionSuggestionId)
+        {
+            var suggestedQuestion = await this.context.QuestionSuggestions.FirstOrDefaultAsync(q => q.Id == questionSuggestionId);
+
+            if (suggestedQuestion == null)
+            {
+                return;
+            }
+
+            var mappedApprovedQuestion = mapper.Map<Question>(suggestedQuestion);
+
+            await this.context.Questions.AddAsync(mappedApprovedQuestion);
+            this.context.QuestionSuggestions.Remove(suggestedQuestion);
+
+            await this.context.SaveChangesAsync();
+        }
+
+        public async Task RemoveSuggestion(int questionSuggestionId)
+        {
+            var suggestedQuestion = await this.context.QuestionSuggestions.FirstOrDefaultAsync(q => q.Id == questionSuggestionId);
+
+            if (suggestedQuestion == null)
+            {
+                return;
+            }
+
+            this.context.QuestionSuggestions.Remove(suggestedQuestion);
+
+            await this.context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<QuestionSuggestion>> GetAllSuggestions()
+        {
+            var allSuggestions = await this.context.QuestionSuggestions.
+                Include(u=>u.User)
+                .Include(q=>q.Quiz)
+                .OrderBy(s=>s.SuggestedOn)
+                .ToListAsync();
+
+            return allSuggestions;
+        }
+    }
+}
---
+++ src/Quizaldo.Services/Interfaces/IQuestionSuggestionService.cs
@@ -0,0 +1,19 @@
+﻿using Quizaldo.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizaldo.Services.Interfaces
+{
+    public interface IQuestionSuggestionService
+    {
+        Task SuggestQuestion(QuestionSuggestion questionSuggestion, string username);
+
+        Task ApproveSuggestion(int questionSuggestionId);
+
+        Task RemoveSuggestion(int questionSuggestionId);
+
+        Task<IEnumerable<QuestionSuggestion>> GetAllSuggestions();
+    }
+}
