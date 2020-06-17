7894c03222b124510da358b803cb24c1a0a2550e
Valentinles <valentinvn@abv.bg>
2020-06-17 12:41:02 +0300

    QuestionSuggestion controller added

---
+++ src/Quizaldo.Web/Controllers/QuestionSuggestionController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Quizaldo.Common.ViewModels;
+using Quizaldo.Models;
+using Quizaldo.Services.Interfaces;
+
+namespace Quizaldo.Web.Controllers
+{
+    public class QuestionSuggestionController : Controller
+    {
+        private readonly IQuestionSuggestionService questionSuggestionService;
+        private readonly IQuizService quizService;
+        private readonly IMapper mapper;
+
+        public QuestionSuggestionController(IQuestionSuggestionService questionSuggestionService, IQuizService quizService, IMapper mapper)
+        {
+            this.questionSuggestionService = questionSuggestionService;
+            this.quizService = quizService;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SuggestQuestion()
+        {
+            var quizzes = (await this.quizService.AllQuizzes())
+                .Select(mapper.Map<AllQuizzesViewModel>);
+
+            ViewData["Quizzes"] = new SelectList(quizzes, "Id", "Name");
+
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SuggestQuestion(SuggestQuestionBindingModel questionSuggestion, string username)
+        {
+            var suggestQuestion = mapper.Map<QuestionSuggestion>(questionSuggestion);
+
+            await this.questionSuggestionService.SuggestQuestion(suggestQuestion, this.User.Identity.Name);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public async Task<IActionResult> ApproveSuggestedQuestion(int id)
+        {
+            await this.questionSuggestionService.ApproveSuggestion(id);
+
+            return RedirectToAction("AllSuggestions", "QuestionSuggestion");
+        }
+
+        public async Task<IActionResult> RemoveSuggestedQuestion(int id)
+        {
+            await this.questionSuggestionService.RemoveSuggestion(id);
+
+            return RedirectToAction("AllSuggestions", "QuestionSuggestion");
+        }
+
+        public async Task<IActionResult> AllSuggestions()
+        {
+            var suggestions = (await this.questionSuggestionService.GetAllSuggestions())
+                .Select(mapper.Map<AllQuestionSuggestionsViewModel>);
+
+            return this.View(suggestions);
+        }
+
+    }
+}
